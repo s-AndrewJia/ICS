59aa73706f882b4ed91edade45d3d8dc2bf8f69d
s-AndrewJia <75217519+s-AndrewJia@users.noreply.github.com>
2022-01-07 21:36:09 -0500

    Delete ICS Grade 11 directory

--- ICS Grade 11/Exercises/001-Hello-World/Main.java
+++
@@ -1,7 +0,0 @@
-class Main {
-  //Our Hello World program
-  //Andrew Jia
-  public static void main(String[] args) {
-    System.out.println("Hello, World");
-  }
-}
--- ICS Grade 11/Exercises/302-Method-dieRoll-no-return/Main.java
+++
@@ -1,9 +0,0 @@
-class Main {
-  public static void main(String args[]){
-    dieRoll();
-  }
-  //put your method here 
-  public static void dieRoll()  {
-    System.out.println("You rolled a " + (int)Math.floor(Math.random() * 6 + 1) + "!");
-  }
-}
